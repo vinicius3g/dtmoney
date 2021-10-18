a110149c676bd8339ef5c084dfe2ec33b1951c75
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2021-10-18 20:19:37 -0300

    Add components in APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
 
-
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState<boolean>(false);
+
+  function hanldeOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function hanldeCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <>
+      <Header openOnNewTransactionModal={hanldeOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={hanldeCloseNewTransactionModal}
+      />
+      
       <GlobalStyle />
-    </div>
+    </>
   );
 }
