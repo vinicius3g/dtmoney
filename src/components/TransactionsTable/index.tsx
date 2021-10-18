aaad50fd541ebb5bccb89af3a13641c22d8ec803
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2021-10-18 20:17:51 -0300

    Transaction component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get("/transactions").then((res) => console.log(res.data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvovimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.100,00</td>
+            <td>casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
